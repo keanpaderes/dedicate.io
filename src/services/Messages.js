affb90b1f0257014b42664e8fa5cb513aed7d2e1
Kean Paderes <kean.paderes@whitewidget.com>
2019-02-12 14:37:53 +0800

    feat(msg): Adds Valentines message

--- src/services/Messages.js
+++ src/services/Messages.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 export default {
-  'qLEux':  'This is a test message for Eunice',// Eunice
+  'qLEux':  'Sana magustuhan mo pakulo ko hehehe. '
+    + 'I\'m so proud of you. Lagi mong tatandaan na '
+    + 'susuportahan kita sa lahat ng endeavors mo sa buhay. '
+    + 'Just remember to always pray and trust in him.',// Eunice
   'ilqjx':  'This is a test message for Eugene',// Eugene
   'OgqmF':  'This is a test message for Lui',// Lui
   'mExFI':  'This is a test message for Arell',// Arell
@@ -14,7 +17,7 @@ export default {
   'vYmDj':  'This is a test message for JC',// JC
   'OJoxH':  'This is a test message for Eugene',// Eugene
   'YZOEU':  'This is a test message for Lyka',// Lyka
-  'AYiES':  'This is a test message for Gege ',// Gege 
+  'AYiES':  'This is a test message for Gege ',// Gege
   'veeDT':  'This is a test message for Angic',// Angic
   'GMvPe':  'This is a test message for Danika',// Danika
 }
--- src/views/MessagePage.vue
+++ src/views/MessagePage.vue
@@ -1,20 +1,73 @@
 <template>
   <div class="about">
     <template v-if="this.messageKey === 'qLEux'">
-      <!-- <div class="wrapper">
-        <div class="heart">
-        </div>
-        <div class="message"><p>
-          {{message}}
-        </p></div>
-      </div> -->
-      <div class="start-button">
-        <h2>Click on the circle</h2>
+      <div v-bind:class="{
+        'message-header': true,
+        'message-header-before': !isEndAnimation,
+        'message-header-after': isEndAnimation,
+        }">
+        <transition
+          name="fader"
+          mode="out-in">
+          <template v-if="!isEndAnimation">
+            <h1>Click on the circle</h1>
+          </template>
+          <template v-else>
+            <div>
+              <div class="center-message">
+                <div class="container">
+                  <div class="small-hearts anim1">&hearts;</div>
+                </div>
+                <div class="container">
+                  <div class="small-hearts anim2">&hearts;</div>
+                </div>
+                <div class="container">
+                  <div class="small-hearts anim3">&hearts;</div>
+                </div>
+                <div class="container">
+                  <div class="small-hearts anim4">&hearts;</div>
+                </div>
+              </div>
+              <div class="center-message">
+                <h2>
+                  Happy Valentine's <br/> Day,
+                  <span class='message-header-highlight'>Eunice</span>!
+                </h2>
+                <span>{{ message }}</span>
+                <h2>I love you so much!</h2>
+              </div>
+            </div>
+          </template>
+        </transition>
       </div>
+      <!-- <div v-else>
+        <div class="center-after">
+          <div class="container">
+            <div class="small-hearts anim1">&hearts;</div>
+          </div>
+          <div class="container">
+            <div class="small-hearts anim2">&hearts;</div>
+          </div>
+          <div class="container">
+            <div class="small-hearts anim3">&hearts;</div>
+          </div>
+          <div class="container">
+            <div class="small-hearts anim4">&hearts;</div>
+          </div>
+          <h1>Happy Valentine's Day, Eunice!</h1>
+        </div>
+      </div> -->
       <div v-bind:class="{
         'heart': true,
-        'heart-anim': isStartAnimation}" @click="startHeartAnimation">
+        'heart-anim': isStartAnimation,
+        'heart-end': isEndAnimation,
+        }" @click="startHeartAnimation">
       </div>
+      <!-- <div class="message-box" v-if="isEndAnimation">
+        <div class="message-box-container">
+          <span>{{ message }}</span>
+        </div>
+      </div> -->
     </template>
     <template v-else>
       <h1>{{ message }}</h1>
@@ -32,7 +85,8 @@ export default {
   ],
   data() {
     return {
-      isStartAnimation: false
+      isStartAnimation: false,
+      isEndAnimation: false,
     };
   },
   computed: {
@@ -41,10 +95,26 @@ export default {
     },
   },
   methods: {
-     startHeartAnimation() {
-       console.log('clicked');
-       this.isStartAnimation = true;
-     }
-  }
+    startHeartAnimation() {
+      if (this.messageKey === 'qLEux' && !this.isEndAnimation) {
+        console.log('clicked');
+        this.isStartAnimation = true;
+        document.body.className += ' html-anim';
+        this.isEndAnimation = true;
+        setTimeout(() => {
+          document.body.className = ' html-bg-end';
+          this.isStartAnimation = false;
+        }, 1000);
+      }
+    },
+  },
+  mounted() {
+    if (this.messageKey === 'qLEux') {
+      document.body.className += ' html-bg';
+    }
+  },
+  destroyed() {
+    document.body.className = '';
+  },
 };
 </script>
