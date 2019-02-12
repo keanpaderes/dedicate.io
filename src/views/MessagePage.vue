f973ed28d1809efb172c2ab7d2ab691d32b0b018
Kean Paderes <kean.paderes@gmail.com>
2019-02-12 08:53:49 +0800

    feat(button): Adds click to animate

--- src/views/MessagePage.vue
+++ src/views/MessagePage.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="about">
     <template v-if="this.messageKey === 'qLEux'">
-      <div class="wrapper">
+      <!-- <div class="wrapper">
         <div class="heart">
         </div>
         <div class="message"><p>
           {{message}}
         </p></div>
+      </div> -->
+      <div class="start-button">
+        <h2>Click on the circle</h2>
+      </div>
+      <div v-bind:class="{
+        'heart': true,
+        'heart-anim': isStartAnimation}" @click="startHeartAnimation">
       </div>
     </template>
     <template v-else>
@@ -23,10 +30,21 @@ export default {
   props: [
     'messageKey',
   ],
+  data() {
+    return {
+      isStartAnimation: false
+    };
+  },
   computed: {
     message() {
       return Messages[this.messageKey];
     },
   },
+  methods: {
+     startHeartAnimation() {
+       console.log('clicked');
+       this.isStartAnimation = true;
+     }
+  }
 };
 </script>
