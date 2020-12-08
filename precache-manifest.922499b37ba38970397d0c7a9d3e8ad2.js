2478848510fc1218fa4ee4bae4172fdced9004f7
Kean Paderes <kean.paderes@gmail.com>
2020-12-08 09:10:24 +0800

    b469f02

---
+++ precache-manifest.922499b37ba38970397d0c7a9d3e8ad2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ad2b2177ab411925da",
+    "url": "/dedicate.io/css/app.224c2dff.css"
+  },
+  {
+    "revision": "59ad2b2177ab411925da",
+    "url": "/dedicate.io/js/app.a1dfd36a.js"
+  },
+  {
+    "revision": "b608c00860c7a4667522",
+    "url": "/dedicate.io/css/chunk-vendors.56a40a11.css"
+  },
+  {
+    "revision": "b608c00860c7a4667522",
+    "url": "/dedicate.io/js/chunk-vendors.12034172.js"
+  },
+  {
+    "revision": "f484c0c30106256de757",
+    "url": "/dedicate.io/js/create.99ea639a.js"
+  },
+  {
+    "revision": "92f121d7b85af88fa133",
+    "url": "/dedicate.io/js/message.449fcc2e.js"
+  },
+  {
+    "revision": "58d53757a378556c8e0d00111867aaf6",
+    "url": "/dedicate.io/img/logo_inversed.58d53757.png"
+  },
+  {
+    "revision": "fcd4023753d352505bb16c75d6bae081",
+    "url": "/dedicate.io/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dedicate.io/robots.txt"
+  }
+];
